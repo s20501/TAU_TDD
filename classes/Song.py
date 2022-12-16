a03fc82b082b4e4d335c137f3900d05d4b58cea2
s20501 <s20501@pjwstk.edu.pl>
2022-12-16 13:29:49 +0100

    Song

---
+++ classes/Song.py
@@ -0,0 +1,35 @@
+class Song:
+    def __init__(self, song_path):
+        song = []
+
+        with open(song_path, 'r') as f:
+            for line in f:
+                if line != '\n':
+                    song.append(line)
+        self.song = song
+
+    def get_verse(self, n):
+        if n < 1 or n > len(self.song):
+            raise ValueError("Invalid verse number")
+        else:
+            return self.song[n - 1]
+
+    def get_verse_range(self, start, end):
+        song_len = len(self.song)
+        if start < 1 or start > song_len or end > song_len or start > end:
+            raise ValueError("Invalid verse number")
+
+        result = []
+
+        for i in range(start, end + 1):
+            result.append(self.get_verse(i))
+
+        return result
+
+    def get_full_song(self):
+        return self.song
+
+
+if __name__ == '__main__':
+    new_song = Song("../song.txt")
+    print(new_song.get_verse_range(3, 2))
---
+++ test/test_song.py
@@ -0,0 +1,44 @@
+import unittest
+
+from classes.Song import Song
+
+
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        self.song = Song('../song.txt')
+
+    def test_get_verse(self):
+        verse = self.song.get_verse(1)
+        self.assertEqual(verse, "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n")
+
+    def test_get_verse_invalid(self):
+        with self.assertRaises(ValueError):
+            self.song.get_verse(0)
+
+    def test_get_verse_range(self):
+        verses = self.song.get_verse_range(1, 3)
+        self.assertEqual(verses,
+                         ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n",
+                          "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\n",
+                          "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"])
+
+    def test_get_verse_range_invalid_start(self):
+        with self.assertRaises(ValueError):
+            self.song.get_verse_range(0, 3)
+
+    def test_get_verse_range_invalid_end(self):
+        with self.assertRaises(ValueError):
+            self.song.get_verse_range(3, 1)
+
+    def test_get_full_song_long(self):
+        song = self.song.get_full_song()
+        self.assertEqual(len(song), 12)
+
+    def test_get_full_song_verse(self):
+        song = self.song.get_full_song()
+        self.assertEqual(song[1],
+                         "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
